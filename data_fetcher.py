61c0c89496f7b45c25ad013e243ad663214d3be5
Neeraj Bhatia <57321215+seneeraj@users.noreply.github.com>
2025-05-30 21:25:42 +0530

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,26 +1,9 @@
-from playwright.sync_api import sync_playwright
-import json
+from nsepython import nse_optionchain_scrapper
 
 def fetch_nifty_data():
-    url = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
-
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-        page.set_extra_http_headers({
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-            "Accept": "application/json",
-            "Referer": "https://www.nseindia.com"
-        })
-
-        try:
-            page.goto("https://www.nseindia.com", timeout=10000)
-            page.wait_for_timeout(2000)  # 2 sec wait
-            response = page.request.get(url)
-            data = response.json()
-            browser.close()
-            return data
-        except Exception as e:
-            print(f"⚠️ Error fetching data with Playwright: {e}")
-            browser.close()
-            return {}
+    try:
+        data = nse_optionchain_scrapper("NIFTY")
+        return data
+    except Exception as e:
+        print("❌ Error fetching data from NSE:", e)
+        return {}
