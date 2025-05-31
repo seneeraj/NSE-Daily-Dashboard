dc77279aa5a516f10f14c05c5989d4affd5c8003
Neeraj Bhatia <57321215+seneeraj@users.noreply.github.com>
2025-05-31 09:28:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from data_fetcher import fetch_nifty_data
 from chart_generator import plot_open_interest
-from emailer import send_email  # if using
 
 def run():
     data = fetch_nifty_data()
     if data:
         plot_open_interest(data)
-        # send_email(...)  # optional step
     else:
         print("No data received.")
 
